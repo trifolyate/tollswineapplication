bdaa979266b1c01272c3ef12e7aadf356757fadf
ashraf-rp <21008946@myrp.edu.sg>
2023-07-18 22:10:01 +0800

    test 5

--- retrievebottle.php
+++ retrievebottle.php
@@ -1,14 +1,11 @@
 <?php
-// if (!empty($_POST['id'])) {
-//     $id = $_POST['id'];
-    $con = mysqli_connect("us-cdbr-east-06.cleardb.net", "b34e5df2471635", "6ffed3a5", "heroku_eb7517145b609d1");
-    $result = array();
-    $result['data'] = array();
-    $select = "SELECT * from wine_label where inbound_list_id = '693826083' and status_completed = 0 ";
-    $response = mysqli_query($con,$select);
-    if (mysqli_num_rows($response) != 0) {
-    while($row = mysqli_fetch_array($response))
-    {
+$con = mysqli_connect("us-cdbr-east-06.cleardb.net", "b34e5df2471635", "6ffed3a5", "heroku_eb7517145b609d1");
+$result = array();
+$result['data'] = array();
+$select = "SELECT * from wine_label where inbound_list_id = '693826083' and status_completed = 0 ";
+$response = mysqli_query($con, $select);
+if (mysqli_num_rows($response) != 0) {
+    while ($row = mysqli_fetch_array($response)) {
         $index['wine_name'] = $row['0'];
         $index['vintage'] = $row['1'];
         $index['region_of_production'] = $row['2'];
@@ -20,15 +17,13 @@
         $index['bottle_information'] = $row['8'];
         $index['alcohol_content'] = $row['9'];
 
-        array_push($result['data'],$index);
+        array_push($result['data'], $index);
     }
-    } else $result = array("status" => "failed","message" => "Bottles Not Found");
+} else
+    $result = array("status" => "failed", "message" => "Bottles Not Found");
 
-    $result['success'] = "1";
-    echo json_encode($result, JSON_PRETTY_PRINT);
-    mysqli_close($con);
-// } else
-//     $result = array("status" => "failed", "message" => "Barcode ID Needed");
+$result['success'] = "1";
 
-// echo json_encode($result, JSON_PRETTY_PRINT);
+echo json_encode($result, JSON_PRETTY_PRINT);
+mysqli_close($con);
 ?>
