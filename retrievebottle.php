4893936a51bf3774ba76dc180620339f50ce5549
ashraf-rp <21008946@myrp.edu.sg>
2023-07-18 22:39:53 +0800

    cry

--- retrievebottle.php
+++ retrievebottle.php
@@ -7,20 +7,7 @@
 
     while($row = mysqli_fetch_array($response))
     {
-        $index['alcohol_content'] = $row['0'];
-        $index['bottle_information'] = $row['1'];
-        $index['brand_name'] = $row['2'];
-        $index['grape_variety'] = $row['3'];
-        $index['importer_information'] = $row['4'];
-        $index['model_name'] = $row['5'];
-        $index['quantity'] = $row['6'];
-        $index['region_of_production'] = $row['7'];
-        $index['status_completed'] = $row['8'];
-        $index['vintage'] = $row['9'];
-        $index['wine_name'] = $row['10'];
-        $index['inbound_list_id'] = $row['11'];
-        $index['wine_label_id'] = $row['12'];
-        $index['model_number'] = $row['13'];
+        $index['brand_name'] = $row['0'];
 
         array_push($result['data'],$index);
     }
