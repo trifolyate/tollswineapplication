a7399e91120c0a2c3d8cf42308f953dd58a08bbe
ashraf-rp <21008946@myrp.edu.sg>
2023-07-18 21:59:20 +0800

    test 2

--- retrievebottle.php
+++ retrievebottle.php
@@ -1,30 +1,31 @@
 <?php
 // if (!empty($_POST['id'])) {
-    // $id = $_POST['id'];
-    $result = array();
+//     $id = $_POST['id'];
     $con = mysqli_connect("us-cdbr-east-06.cleardb.net", "b34e5df2471635", "6ffed3a5", "heroku_eb7517145b609d1");
-    if ($con) {
-        $sql = "select * from wine_label where wine_label_id = '572038408' AND status_completed = 0";
-        $res = mysqli_query($con, $sql);
-        if (mysqli_num_rows($res) != 0) {
-            while ($row = mysqli_fetch_array($response)) {
-                $index['wine_name'] = $row['0'];
-                $index['vintage'] = $row['1'];
-                $index['region_of_production'] = $row['2'];
-                $index['quantity'] = $row['3'];
-                $index['model_name'] = $row['4'];
-                $index['importer_information'] = $row['5'];
-                $index['grape_variety'] = $row['6'];
-                $index['brand_name'] = $row['7'];
-                $index['bottle_information'] = $row['8'];
-                $index['quantity'] = $row['9'];
-                $index['alcohol_content'] = $row['10'];
-                array_push($result['data'], $index);
-            }
-        } else
-            $result = array("status" => "failed", "message" => "Empty Results");
-    } else
-        $result = array("status" => "failed", "message" => "Connection to DataBase Failed");
+    $result = array();
+    $result['data'] = array();
+    $select = "SELECT * from wine_label where inbound_list_id = '693826083' and status_completed = 0 ";
+    $response = mysqli_query($con,$select);
+
+    while($row = mysqli_fetch_array($response))
+    {
+        $index['id'] = $row['0'];
+        $index['arrival_date'] = $row['1'];
+        $index['departure_date'] = $row['2'];
+        $index['exporter'] = $row['3'];
+        $index['handling_agent'] = $row['4'];
+        $index['importer'] = $row['5'];
+        $index['permit_no'] = $row['6'];
+        $index['status_completed'] = $row['7'];
+        $index['t_cartons'] = $row['8'];
+        $index['t_quantity'] = $row['9'];
+
+        array_push($result['data'],$index);
+    }
+
+    $result['success'] = "1";
+    echo json_encode($result, JSON_PRETTY_PRINT);
+    mysqli_close($con);
 // } else
 //     $result = array("status" => "failed", "message" => "Barcode ID Needed");
 
