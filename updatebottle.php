fd020342902ad79adda404d6078b9d62f655557e
ashraf-rp <21008946@myrp.edu.sg>
2023-07-19 03:55:51 +0800

    time to sleep

--- updatebottle.php
+++ updatebottle.php
@@ -4,7 +4,7 @@ if (isset($_POST['wine_label_id'])) {
     $con = mysqli_connect("us-cdbr-east-06.cleardb.net", "b34e5df2471635", "6ffed3a5", "heroku_eb7517145b609d1");
 
     // Update the status_completed column to 1
-    $updateQuery = "UPDATE wine_label SET status_completed = 1 WHERE wine_label_id = '$wineLabelId'";
+    $updateQuery = "UPDATE wine_label SET status_completed = 1 WHERE id = '$wineLabelId'";
     mysqli_set_charset($con, "utf8");
     $updateResult = mysqli_query($con, $updateQuery);
 
