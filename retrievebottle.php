3acf45df7dfcb6bede0059cb2a19ffc54aedcce7
ashraf-rp <21008946@myrp.edu.sg>
2023-07-18 22:42:24 +0800

    finally

--- retrievebottle.php
+++ retrievebottle.php
@@ -2,7 +2,7 @@
     $con = mysqli_connect("us-cdbr-east-06.cleardb.net", "b34e5df2471635", "6ffed3a5", "heroku_eb7517145b609d1");
     $result = array();
     $result['data'] = array();
-    $select = "SELECT * from wine_label";
+    $select = "SELECT * from wine_label where status_completed = 0";
     $response = mysqli_query($con,$select);
 
     while($row = mysqli_fetch_array($response))
