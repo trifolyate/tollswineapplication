0094c1a93294423ab1ff92644635aa1374824a6c
ashraf-rp <21008946@myrp.edu.sg>
2023-07-19 00:34:24 +0800

    back to original code

--- retrievebottle.php
+++ retrievebottle.php
@@ -1,10 +1,10 @@
 <?php
-// if (isset($_POST['id'])) {
+if (isset($_POST['id'])) {
     $id = $_POST['id'];
     $con = mysqli_connect("us-cdbr-east-06.cleardb.net", "b34e5df2471635", "6ffed3a5", "heroku_eb7517145b609d1");
     $result = array();
     $result['data'] = array();
-    $select = "SELECT * from wine_label where status_completed = 0 ";
+    $select = "SELECT * from wine_label where inbound_list_id = '" . $_POST['id'] . "' and status_completed = 0 ";
     mysqli_set_charset($con, "utf8");
     $response = mysqli_query($con, $select);
     while ($row = mysqli_fetch_array($response)) {
@@ -24,7 +24,7 @@
 
         $result['success'] = "1";
     }
-// } else $result = array("status" => "failed","message" => "Barcode ID needed");
+} else $result = array("status" => "failed","message" => "Barcode ID needed");
 
 
 echo json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
