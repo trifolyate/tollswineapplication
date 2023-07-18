f8495cb3460c7e90a4b5f3c058672442ac96310d
ashraf-rp <21008946@myrp.edu.sg>
2023-07-18 22:16:06 +0800

    test 6

--- retrievebottle.php
+++ retrievebottle.php
@@ -6,16 +6,16 @@ $select = "SELECT * from wine_label where inbound_list_id = '693826083' and stat
 $response = mysqli_query($con, $select);
 if (mysqli_num_rows($response) != 0) {
     while ($row = mysqli_fetch_array($response)) {
-        $index['wine_name'] = $row['0'];
-        $index['vintage'] = $row['1'];
-        $index['region_of_production'] = $row['2'];
-        $index['quantity'] = $row['3'];
-        $index['model_name'] = $row['4'];
-        $index['importer_information'] = $row['5'];
-        $index['grape_variety'] = $row['6'];
-        $index['brand_name'] = $row['7'];
-        $index['bottle_information'] = $row['8'];
-        $index['alcohol_content'] = $row['9'];
+        $index['alcohol_content'] = $row['0'];
+        $index['bottle_information'] = $row['1'];
+        $index['brand_name'] = $row['2'];
+        $index['grape_variety'] = $row['3'];
+        $index['importer_information'] = $row['4'];
+        $index['model_name'] = $row['5'];
+        $index['quantity'] = $row['6'];
+        $index['region_of_production'] = $row['7'];
+        $index['vintage'] = $row['8'];
+        $index['wine_name'] = $row['9'];
 
         array_push($result['data'], $index);
     }
