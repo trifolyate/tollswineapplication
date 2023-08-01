142c644838ed286f4b0cead69fb81bc1aeb16541
ashraf-rp <21008946@myrp.edu.sg>
2023-08-01 19:34:39 +0800

    test

--- checkextractedbottletext.php
+++ checkextractedbottletext.php
@@ -60,9 +60,11 @@ if (isset($_POST['id']) && isset($_POST['text'])) {
     if (count($result['data']) > 0) {
         $result['success'] = "1";
         $result['extractedtext'] = $text;
+        $result['similarityRate'] = $similarity;
     } else {
         $result['success'] = "0";
         $result['extractedtext'] = $text;
+        $result['similarityRate'] = $similarity;
     }
 } else {
     $result = array("status" => "failed", "message" => "Barcode ID and Text needed");
