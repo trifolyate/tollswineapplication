1ea17b2b054c9e77ed9a12798e24238a6242a991
ashraf-rp <21008946@myrp.edu.sg>
2023-08-01 16:08:12 +0800

    only 10 percent :<

--- checkextractedbottletext.php
+++ checkextractedbottletext.php
@@ -30,7 +30,7 @@ if (isset($_POST['id']) && isset($_POST['text'])) {
         $similarity = similarityRatio($text, $wine_name);
 
         // If similarity is greater than or equal to 0.5 (50% similarity), add the data to the result
-        if ($similarity >= 0.2) {
+        if ($similarity >= 0.1) {
             $index['wine_name'] = $wine_name;
             $index['vintage'] = $row['10'];
             $index['region_of_production'] = $row['8'];
